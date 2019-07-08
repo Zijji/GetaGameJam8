4fdd43e79446fd3ea4a29868fe94db3fbb58ae9c
Sirmaka <ldjr22@gmail.com>
2019-07-09 01:12:18 +1000

    added spooky eye things

---
+++ GetaGameJam8/Assets/ColourChange.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourChange : MonoBehaviour
+{
+    private GameObject dreamstatecont = null;
+    private int dreamState = -1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dreamstatecont != null)
+        {
+            dreamState = dreamstatecont.GetComponent<DreamStateController>().GetDreamState();
+        }
+
+
+        
+        if (dreamState == 0)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(255f, 0f, 0f);
+
+        }
+        else if (dreamState == 1)
+        {
+            gameObject.GetComponent<SpriteRenderer>().color = new Color(0f, 0f, 0f);
+        }
+    }
+    
+}
---
+++ GetaGameJam8/Assets/EyeOpener.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeOpener : MonoBehaviour
+{
+    private GameObject dreamstatecont = null;
+    private int dreamState = -1;
+    public Sprite wokeSprite = null;
+    public Sprite sleepSprite = null;
+    private SpriteRenderer getSprRen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        dreamstatecont = GameObject.Find("DreamStateController");
+        getSprRen = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Dreamstate controller
+        if (dreamstatecont != null)
+        {
+            dreamState = dreamstatecont.GetComponent<DreamStateController>().GetDreamState();
+        }
+
+        if (dreamState == 0)
+        {
+            getSprRen.sprite = wokeSprite;
+        }
+        else if (dreamState == 1)
+        {
+            getSprRen.sprite = sleepSprite;
+        }
+    }
+}
+
